a3d126d4ca520a008b63a3cef9bc72ddd0fdd9cd
Josiah Campbell <josiahcampbell64@gmail.com>
2017-12-06 23:18:35 -0500

    Add null constraint to password on create

--- lib/allowances/accounts/user.ex
+++ lib/allowances/accounts/user.ex
@@ -23,7 +23,7 @@ defmodule Allowances.Accounts.User do
   def create_changeset(%User{} = user, attrs) do
     user
     |> cast(attrs, [:email, :password])
-    |> validate_required([:email])
+    |> validate_required([:email, :password])
     |> unique_email
     |> validate_password(:password)
     |> put_pass_hash
@@ -35,10 +35,6 @@ defmodule Allowances.Accounts.User do
     |> unique_constraint(:email)
   end
 
-  # In the function below, strong_password? just checks that the password
-  # is at least 8 characters long.
-  # See the documentation for NotQwerty123.PasswordStrength.strong_password?
-  # for a more comprehensive password strength checker.
   defp validate_password(changeset, field, options \\ []) do
     validate_change(changeset, field, fn _, password ->
       case strong_password?(password) do
@@ -49,8 +45,7 @@ defmodule Allowances.Accounts.User do
   end
 
   # If you are using Argon2 or Pbkdf2, change Bcrypt to Argon2 or Pbkdf2
-  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes:
-      %{password: password}} = changeset) do
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
     change(changeset, Comeonin.Bcrypt.add_hash(password))
   end
   defp put_pass_hash(changeset), do: changeset
