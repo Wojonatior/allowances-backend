17f9f0253c55292debfe8dc3fc5829aebe8fa736
Josiah Campbell <josiahcampbell64@gmail.com>
2018-01-29 19:17:19 -0500

    users -> all_users

--- lib/allowances_web/schema.ex
+++ lib/allowances_web/schema.ex
@@ -10,12 +10,13 @@ defmodule AllowancesWeb.Schema do
   end
 
   query do
-    field :all_users, non_null(list_of(non_null(:user))) do
-      resolve &UserResolver.all_users/3
-    end
     field :user, :user do
       arg :id, :id, description: "Unique user identifier"
       resolve &UserResolver.user/3
+    end
+
+    field :users, non_null(list_of(non_null(:user))) do
+      resolve &UserResolver.all_users/3
     end
   end
 end
