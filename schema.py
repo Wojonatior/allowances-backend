040c20f9204aed462872ae3fd766c71dd5694380
Jarek Wojciechowski <jarekwojo@gmail.com>
2018-04-04 19:04:17 -0400

    proress on graphql schema

--- schema.py
+++ schema.py
@@ -5,15 +5,19 @@ from models import db_session, Department as DepartmentModel, Employee as Employ
 
 
 class Department(SQLAlchemyObjectType):
+    id = graphene.ID()
+    name = graphene.String()
     class Meta:
         model = DepartmentModel
 
 
 class Employee(SQLAlchemyObjectType):
+    id = graphene.ID()
     class Meta:
         model = EmployeeModel
 
 class User(SQLAlchemyObjectType):
+    id = graphene.ID()
     class Meta:
         model = UserModel
 
@@ -21,19 +25,24 @@ class Query(graphene.ObjectType):
     departments = graphene.List(Department)
     employees = graphene.List(Employee)
     users = graphene.List(User)
+    department = graphene.Field(Department,
+                                id=graphene.String())
 
     def resolve_departments(self, info):
         query = Department.get_query(info)
         return query.all()
 
-    def resolve_employee(self, info):
+    def resolve_employees(self, info):
         query = Employee.get_query(info)
         return query.all()
 
     def resolve_users(self, info):
         query = User.get_query(info)
         return query.all()
-    
+
+    def resolve_department(self, info, id):
+        query = Department.get_query(info)
+        return query.all()
 
 
 schema = graphene.Schema(query=Query)
