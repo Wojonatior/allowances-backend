32585c3323b5f549d05a432fa247e82313ba5f4a
Jarek Wojciechowski <jarekwojo@gmail.com>
2018-04-09 16:16:13 -0400

    remove the old new_user endpoint

--- api.py
+++ api.py
@@ -108,22 +108,5 @@ def get_auth_token():
     token = g.user.generate_auth_token(600)
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
-@app.route('/api/users', methods=['POST'])
-def new_user():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400)    # missing arguments
-    if User.query.filter_by(username=username).first() is not None:
-        abort(400)    # existing user
-    user = User(username=username)
-    user.hash_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return (jsonify({'username': user.username}), 201,
-            {'Location': url_for('get_user', id=user.id, _external=True)})
-
-
-
 if(__name__ == '__main__'):
     app.run(debug=True)
